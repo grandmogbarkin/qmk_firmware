166c753c0ec5d5b5322b4ba65e6fdd20881d7fda
Constantinos Neophytou <constantinos@neophytou.net>
2020-03-25 14:29:20 +0000

    Example secrets file

---
+++ secrets_example.h
@@ -0,0 +1,17 @@
+typedef struct {
+  char str[5];
+} segment;
+
+static segment keycode_vignere_enc[] = {
+  {"6HRS"}, {"LqZm"}, {"R\\dK"}, {"F0ax"}, {"o3me"}, {"I5iM"}, {"idkf"}, {"uYOG"}, {"8~q "},
+  {"DcIv"}, {"?hay"}, {"mduk"}, {"lqSy"}, {"NmmO"}, {"MN f"}, {"Xw5G"}, {"G6p7"}, {"dWXf"},
+  {"m5Um"}, {"vCEU"}, {"O/Vm"}, {"E~kv"}, {"nALg"}, {"Lcl0"}, {"3Mp\""}, {"RWYc"}, {"rUz8"},
+  {"aJYk"}, {"QYVl"}, {"4Vvn"}, {"6EsI"}, {"cQJN"}, {"fUwJ"}, {"aRWT"}, {"IC67"}, {"zORB"},
+  {"u6on"}, {"W4uZ"}, {"kWqn"}, {"XY8q"}, {"ehjy"}, {"B`R7"}, {"7SKn"}, {"I0#K"}, {"2cVB"},
+  {"R1Z4"}, {"M8Io"}, {"~xKb"}, {"qby{"}, {"Ia?m"}, {"lrzm"}, {"\\UOz"}, {"jIue"}, {"r\"m "},
+  {"0nb3"}, {"EA?6"}, {"nIHp"}, {"y3AV"}, {"zu2j"}, {"JeI3"}, {"G2Qc"}, {"Kf5E"}, {" D/V"},
+  {"EiKG"}, {"Rrj9"}, {"beZK"}, {"ZElb"}, {"gNaA"}, {"WezJ"}, {"ntIT"}, {"P43k"}, {"x6a6"},
+  {"k3as"}, {"MJaB"}, {"1nOz"}, {"Rg{y"}, {"Gi7H"}, {"faSa"}, {"nohI"}, {"O6DU"}, {"2WjK"},
+  {"n\\x6"}, {"3#ph"}, {"s5nu"}, {"iVTX"}, {"8h9D"}, {"f609"}, {"i#BI"}, {"4Mfe"}, {"6seI"},
+  {"1ywC"}, {"u5qA"}, {"Qdvf"}, {"76nA"}, {"c`6z"}
+};
