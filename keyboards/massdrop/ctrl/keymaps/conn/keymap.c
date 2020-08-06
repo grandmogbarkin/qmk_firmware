ebf2c5611cdcb7341e8792e007b4b8ac44a74fc9
Constantinos Neophytou <1714925+jaguarcy@users.noreply.github.com>
2020-08-06 09:40:09 +0100

    Add backspace to secrets layer

--- keyboards/massdrop/ctrl/keymaps/conn/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/conn/keymap.c
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [2] = LAYOUT(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, \
-        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, \
+        XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX, XXXXXXX, KC_BSPC,   XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   XXXXXXX, XXXXXXX, XXXXXXX, \
         XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
         XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              XXXXXXX, \
@@ -253,6 +253,15 @@ bool process_custom_record_keymap(uint16_t keycode, keyrecord_t *record) {
         shifted = 1;
       } else {
         shifted = 0;
+      }
+      break;
+    case KC_BSPC:
+      if (record->event.pressed) {
+        if (dec_len < 0 && key_len > 0) {
+          key_len--;
+        } else if (dec_len > 0) {
+          dec_len--;
+        }
       }
       break;
     default:
